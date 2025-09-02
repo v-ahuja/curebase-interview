8b5046c9a3797e3a7be0d83a058169e71f679a88
Vineet Ahuja <vineet.ahuja1990@gmail.com>
2025-09-02 18:13:43 +0530

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ const geistMono = Geist_Mono({
 function UsersDialog() {
   return (
     <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
+      {/* <DialogTrigger>Open</DialogTrigger> */}
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Are you absolutely sure?</DialogTitle>
@@ -49,41 +49,43 @@ export default function Home() {
   const [dialogOpen, setDialogOpen] = React.useState(false);
 
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 `}
-    >
-      {/* {dialogOpen && <UsersDialog />} */}
-      <div className="w-4xl space-y-4">
-        <div className="flex flex-row-reverse w-full">
-          <button
-            className="bg-slate-700 text-white text-sm px-2 py-2 rounded-md"
-            onClick={() => setDialogOpen(true)}
-          >
-            Add User
-          </button>
+    <>
+      {dialogOpen && <UsersDialog />}
+      <div
+        className={`${geistSans.className} ${geistMono.className} items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 `}
+      >
+        <div className="w-4xl space-y-4">
+          <div className="flex flex-row-reverse w-full">
+            <button
+              className="bg-slate-700 text-white text-sm px-2 py-2 rounded-md"
+              onClick={() => setDialogOpen(true)}
+            >
+              Add User
+            </button>
+          </div>
+          <table className="table-fixed w-full">
+            <thead
+              className="w-full border border-slate-300 bg-slate-100 rounded-md"
+              key="thead"
+            >
+              <tr className="">
+                <th className="p-2 text-left">First Name</th>
+                <th className="p-2 text-left">Last Name</th>
+                <th className="p-2 text-left">DOB</th>
+                <th className="p-2 text-left">Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="p-2">John</td>
+                <td className="p-2">Doe</td>
+                <td className="p-2">1990-01-01</td>
+                <td className="p-2">123 Main St</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <table className="table-fixed w-full">
-          <thead
-            className="w-full border border-slate-300 bg-slate-100 rounded-md"
-            key="thead"
-          >
-            <tr className="">
-              <th className="p-2 text-left">First Name</th>
-              <th className="p-2 text-left">Last Name</th>
-              <th className="p-2 text-left">DOB</th>
-              <th className="p-2 text-left">Address</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="p-2">John</td>
-              <td className="p-2">Doe</td>
-              <td className="p-2">1990-01-01</td>
-              <td className="p-2">123 Main St</td>
-            </tr>
-          </tbody>
-        </table>
       </div>
-    </div>
+    </>
   );
 }
