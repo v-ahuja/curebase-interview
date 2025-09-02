786fe572dfd0d19469fa27b6a2dbb5283d418714
Vineet Ahuja <vineet.ahuja1990@gmail.com>
2025-09-02 15:40:16 +0530

    update sidebar

--- src/ui/shared-components/layout/sidebar.tsx
+++ src/ui/shared-components/layout/sidebar.tsx
@@ -10,7 +10,7 @@ function isBaselinesV2Enabled() {
 }
 
 const navigation = [
-  { name: "Home", href: "/", icon: PiHouse, current: false },
+  { name: "Users", href: "/", icon: PiHouse, current: false },
   {
     name: "Companies",
     href: "/companies",
